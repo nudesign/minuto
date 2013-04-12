402269f020ef99ad55b949cb4a3ae3c60397f328
Rodrigo Pestana <rodrigo.pest@gmail.com>
2013-04-12 19:06:28 -0300

    added grid

--- helpers/base.rb
+++ helpers/base.rb
@@ -1,8 +1,12 @@
 # If @title is assigned, add it to the page's title.
 def title
-@title ? "#{@title} | festival do minuto" : "festival do minuto"
+  @title ? "#{@title} | festival do minuto" : "festival do minuto"
 end
 
 def partial(page, options={})
-erb page.to_sym, options.merge!(:layout => false)
+  erb page.to_sym, options.merge!(:layout => false)
 end
+
+def development?; ENV['RACK_ENV'].to_sym == :development end
+def production?;  ENV['RACK_ENV'].to_sym == :production  end
+def test?;        ENV['RACK_ENV'].to_sym == :test        end
